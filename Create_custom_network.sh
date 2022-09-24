79ef46e5625131a8c0c6afdc9f0ea043fb3d44e9
durgakiran <kiranfet@gmail.com>
2022-09-24 12:46:11 +0530

    Add additional peers

--- Create_custom_network.sh
+++ Create_custom_network.sh
@@ -2,9 +2,9 @@
 export FABRIC_CFG_PATH=$PWD
 ../bin/configtxgen -profile Raft -channelID talentsprint-sys-channel-outputBlock ./channel-artifacts/genesis.block
 export CHANNEL_NAME=talentsprint
-../bin/configtxgen -profile FourOrgsChannel -outputCreateChannelTx ./channel-artifacts/talentsprint.tx -channelID $CHANNEL_NAME
-../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
-../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
-../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org3MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org3MSP
-../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org4MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org4MSP
+../bin/configtxgen -profile SixOrgsChannel -outputCreateChannelTx ./channel-artifacts/talentsprint.tx -channelID $CHANNEL_NAME
+../bin/configtxgen -profile SixOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+../bin/configtxgen -profile SixOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+../bin/configtxgen -profile SixOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org3MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org3MSP
+../bin/configtxgen -profile SixOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org4MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org4MSP
 . ./Start_docker_Nerwork.sh
